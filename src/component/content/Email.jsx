c802e367e2de5b3b2fe8ac9df7010bf8e6de84ef
ridho808 <syamridho58@gmail.com>
2022-09-13 19:28:45 +0700

    version 2

--- src/component/content/Email.jsx
+++ src/component/content/Email.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
-import { AiFillMail } from 'react-icons/ai'
+import  Axios  from 'axios';
+import React, { useState } from 'react';
+import { AiFillMail } from 'react-icons/ai';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+
+const MySwal = withReactContent(Swal);
 export default function Email({ Email }) {
+    const [Name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [Message,setMessage] = useState('');
+    const [Alert,setAlert] = useState(Boolean);
+
+    const SubmitMsg = async(event)=>{
+        event.preventDefault();
+        const Data = {
+            name : Name,
+            email : email,
+            message : Message
+        }
+        console.log(Data)
+        try {
+            const Post = await Axios.post('https://mailfordoolean.vercel.app/postmsg',Data);
+            if(Post.status === 200){
+                setName('');
+                setMessage('');
+                setEmail('');
+                setAlert(true);
+            }
+        } catch (error) {
+            alert(error);
+            setAlert(false)
+        }
+    }
     return (
         <div data-aos="zoom-in"
             className="max-w-xl w-full backdrop-blur-xl bg-white/60 rounded-lg flex flex-col fixed top-[11%] sm:left-[20%] md:left-[18%] lg:left-[28%] xl:left-[30%] 2xl:left-[32%]">
@@ -13,18 +45,18 @@ export default function Email({ Email }) {
                 <h1 className='text-4xl font-bold text-gray-800 p-2'>Hire me......</h1>
                 <div className='flex flex-row items-center w-full'>
                     <div className='w-full m-3'>
-                        <form className='flex flex-col gap-2 w-full'>
+                        <form className='flex flex-col gap-2 w-full' onSubmit={SubmitMsg}>
                             <div className='flex flex-col justify-evenly gap-2 items-center text-gray-700'>
                                 <lable className="font-bold">Name</lable>
-                                <input type="text" placeholder='Your Name ...' className='border w-full h-[30px] p-2 rounded-md bg-white/70' />
+                                <input value={Name} onChange={(value)=>setName(value.target.value)} type="text" placeholder='Your Name ...' className='border w-full h-[30px] p-2 rounded-md bg-white/70' />
                             </div>
                             <div className='flex flex-col justify-evenly gap-2 items-center text-gray-700'>
                                 <lable className="font-bold">Email</lable>
-                                <input type="text" placeholder='Your Email...' className='border w-full h-[30px] p-2 rounded-md bg-white/70' />
+                                <input value={email} onChange={(value)=>setEmail(value.target.value)} type="text" placeholder='Your Email...' className='border w-full h-[30px] p-2 rounded-md bg-white/70' />
                             </div>
                             <div className='flex flex-col justify-evenly gap-2 items-center text-gray-700'>
                                 <lable className="font-bold">Message</lable>
-                                <textarea type="text" placeholder='Message ...' className='border w-full h-[70px] p-2 rounded-md bg-white/70'></textarea>
+                                <textarea value={Message} onChange={(value)=>setMessage(value.target.value)} type="text" placeholder='Message ...' className='border w-full h-[70px] p-2 rounded-md bg-white/70'></textarea>
                             </div>
                             <button className='bg-sky-500 rounded-xl font-bold w-[150px] h-[40px] flex items-center justify-center mx-auto text-white'>
                                 <AiFillMail size={25} /> SEND
@@ -36,6 +68,11 @@ export default function Email({ Email }) {
                     </div>
                 </div>
             </div>
+            {Alert ? MySwal.fire({
+                title: <strong>SUCCSESS!</strong>,
+                html: <i>Message sent successfully</i>,
+                icon: 'success'
+            }) : null}
         </div>
     )
 }
